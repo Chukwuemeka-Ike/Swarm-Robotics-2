7d7d671815f9cb70015b19865ffce8f4cd5cbc1d
Burak Aksoy <burakaksoy20@gmail.com>
2023-09-25 16:41:11 -0400

    debug

--- computers_install.bash
+++ computers_install.bash
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
-# CUSTOM RELATED
-sudo apt-get install -y sshpass;
 
-sudo apt-get install -y ros-noetic-rqt-ez-publisher;
-sudo apt-get install -y spacenavd;
-sudo apt-get install -y ros-noetic-spacenav-node;
-sudo apt-get install -y ros-noetic-tf2-sensor-msgs;
-sudo apt-get install -y ros-noetic-imu-tools;
-sudo apt-get install -y ros-noetic-imu-pipeline; # for imu_transformer
-sudo apt-get install -y ros-noetic-navigation; # for navigation stack
-sudo apt-get install -y ros-noetic-visualization-tutorials; # rviz python bindings.
+# Check if MY_PASSWORD is set
+if [ -z "$MY_PASSWORD" ]; then
+    echo "Password not set. Exiting..."
+    exit 1
+fi
+
+# CUSTOM RELATED
+echo $MY_PASSWORD | sudo -S apt-get install -y sshpass;
+
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-rqt-ez-publisher;
+echo $MY_PASSWORD | sudo -S apt-get install -y spacenavd;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-spacenav-node;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-tf2-sensor-msgs;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-imu-tools;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-imu-pipeline; # for imu_transformer
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-navigation; # for navigation stack
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-visualization-tutorials; # rviz python bindings.
 
 # PYTHON RELATED
-sudo apt-get install -y python3-pip;
+echo $MY_PASSWORD | sudo -S apt-get install -y python3-pip;
 pip3 install pyserial;
 pip3 install quadprog;
 pip3 install pandas;
@@ -24,11 +31,11 @@ pip3 install matplotlib==3.7.3;
 pip3 install ortools==9.7.2996;
 
 # DINGO RELATED
-sudo apt-get install -y ros-noetic-dingo-desktop;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-dingo-desktop;
 
-sudo apt-get install -y ros-noetic-dingo-simulator;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-dingo-simulator;
 
-sudo apt install ros-noetic-dingo-navigation;
+echo $MY_PASSWORD | sudo -S apt-get install -y ros-noetic-dingo-navigation;
 
 
 ## Building Steps
@@ -45,6 +52,7 @@ git clone https://github.com/burakaksoy/uwb_gazebo_plugin; # only on DESKTOP
 git clone https://github.com/burakaksoy/multiRobotPlanner.git # only on DESKTOP. CK's repo, but cloning my for fork stable testing
 
 cd ..;
+source /opt/ros/noetic/setup.bash;
 catkin_make -DCATKIN_BLACKLIST_PACKAGES='dingo_base;dingo_customization' -DCMAKE_BUILD_TYPE=Release; # on DESKTOP computer 
 # catkin_make -DCATKIN_BLACKLIST_PACKAGES='swarm_gui;arm_gui;arm_msgs;arm_utils;machine_manager;robot_assigner;task_scheduler;ticket_manager' -DCMAKE_BUILD_TYPE=Release; # on Physical Robots
 source devel/setup.bash;
